eb0312191a85f59fa1ef147ff5638a1abfec1bb2
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-05-21 16:14:33 -0400

    sss

--- PongPaddle.java
+++ PongPaddle.java
@@ -1,3 +1,27 @@
 public class PongPaddle {
-    
+    private int x, y, width, height;
+    public PongPaddle(int top, int left, int w, int h){
+        y = top;
+        x = left;
+        w = width;
+        h = height;
+    }
+    public int getBottomY(){
+        return(height + y) - 1;
+    }
+    public int getLeftX(){
+        return x;
+    }
+    public int getRightX(){
+        return (width + x) - 1;
+    }
+    public int getTopY(){
+        return y;
+    }
+    public void moveDown(int d){
+        y = y + d;
+    }
+    public void moveUp(int d){
+        y = y - d;
+    }
 }
